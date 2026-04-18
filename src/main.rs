7bb7742884545e5848a0844686ebf00d2b707598
lillie <onlinechronically@protonmail.com>
2026-04-17 20:39:42 -0400

    add: db connection code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::postgres::PgPoolOptions;
+
+#[tokio::main]
+async fn main() {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(std::env::var("DATABASE_URL").unwrap().as_str())
+        .await
+        .expect("There was an issue connecting to the database");
 }
