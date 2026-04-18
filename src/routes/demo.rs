3be2143dc0bb04457203abc7497acfb6fcbf99b1
lillie <onlinechronically@protonmail.com>
2026-04-18 17:02:47 -0400

    fix: demo assets

--- src/routes/demo.rs
+++ src/routes/demo.rs
@@ -1,24 +1,25 @@
 use axum::{
     Router,
-    http::{HeaderName, header::CONTENT_TYPE},
-    response::AppendHeaders,
+    body::Body,
+    http::header::{self},
+    response::Response,
     routing::get,
 };
 
 use crate::router::AppState;
 
-async fn demo_stylesheet() -> (AppendHeaders<[(HeaderName, &'static str); 1]>, &'static str) {
-    (
-        AppendHeaders([(CONTENT_TYPE, "text/css")]),
-        include_str!("../../demo/style.css"),
-    )
+async fn demo_stylesheet() -> Response {
+    Response::builder()
+        .header(header::CONTENT_TYPE, "text/css")
+        .body(Body::from(include_str!("../../demo/style.css")))
+        .unwrap()
 }
 
-async fn demo_script() -> (AppendHeaders<[(HeaderName, &'static str); 1]>, &'static str) {
-    (
-        AppendHeaders([(CONTENT_TYPE, "text/js")]),
-        include_str!("../../demo/script.js"),
-    )
+async fn demo_script() -> Response {
+    Response::builder()
+        .header(header::CONTENT_TYPE, "text/javascript")
+        .body(Body::from(include_str!("../../demo/script.js")))
+        .unwrap()
 }
 
 pub fn router() -> Router<AppState> {
