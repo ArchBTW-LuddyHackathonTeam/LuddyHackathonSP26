04df61750f0d733793b8f886752a932dcfebc39b
lillie <onlinechronically@protonmail.com>
2026-04-17 21:35:32 -0400

    add: setup flag and clap to main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+use clap::Parser;
 use sqlx::postgres::PgPoolOptions;
+
+#[derive(Parser)]
+struct Args {
+    /// setup flag lol (TODO: proper english)
+    #[arg(short, long)]
+    setup: bool,
+}
 
 #[tokio::main]
 async fn main() {
+    let args = Args::parse();
     let pool = PgPoolOptions::new()
         .max_connections(5)
         .connect(std::env::var("DATABASE_URL").unwrap().as_str())
