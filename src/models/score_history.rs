818787020d03e01e284bb1e8bd58cdd279653375
Ian McGillivary <wiian16@icloud.com>
2026-04-18 01:29:21 -0400

    Fix table in score_history

--- src/models/score_history.rs
+++ src/models/score_history.rs
@@ -12,7 +12,7 @@ pub struct ScoreHistory {
 impl ScoreHistory {
     /// Fetch a single score history entry by its id
     pub async fn from_id(pool: &PgPool, id: Uuid) -> Result<Option<Self>, sqlx::Error> {
-        sqlx::query_as("SELECT id, uploader, created_at, value FROM score WHERE id == $1")
+        sqlx::query_as("SELECT id, uploader, created_at, value FROM score_history WHERE id == $1")
             .bind(id)
             .fetch_optional(pool)
             .await
@@ -21,15 +21,17 @@ impl ScoreHistory {
     // TODO: Add paging
     /// Fetch all score histories by an uploader
     pub async fn by_uploader(pool: &PgPool, uploader: String) -> Result<Vec<Self>, sqlx::Error> {
-        sqlx::query_as("SELECT id, uploader, created_at, value, FROM score where uploader == $1")
-            .bind(uploader)
-            .fetch_all(pool)
-            .await
+        sqlx::query_as(
+            "SELECT id, uploader, created_at, value, FROM score_history where uploader == $1",
+        )
+        .bind(uploader)
+        .fetch_all(pool)
+        .await
     }
 
     /// Insert a new score history entry and return the created record.
     pub async fn create(pool: &PgPool, uploader: String, value: f64) -> Result<Self, sqlx::Error> {
-        sqlx::query_as("INSERT INTO score (uploader, value) VALUES ($1, $2) RETURNING id, uploader, value, created_at")
+        sqlx::query_as("INSERT INTO score_history (uploader, value) VALUES ($1, $2) RETURNING id, uploader, value, created_at")
             .bind(uploader)
             .bind(value)
             .fetch_one(pool)
@@ -38,7 +40,7 @@ impl ScoreHistory {
 
     /// Removes all entries from the table
     pub async fn delete_all(pool: &PgPool) -> Result<u64, sqlx::Error> {
-        sqlx::query("DELETE FROM score")
+        sqlx::query("DELETE FROM score_history")
             .execute(pool)
             .await
             .map(|r| r.rows_affected())
