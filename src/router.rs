11371bde05c0dd76f5a80307f2a95a432665630c
Ian McGillivary <wiian16@icloud.com>
2026-04-18 01:50:13 -0400

    Add /add router

--- src/router.rs
+++ src/router.rs
@@ -1,8 +1,16 @@
-use axum::{Router, routing::{get, post}, extract::{State, Query}, Json};
+use axum::{
+    extract::{Query, State},
+    http::StatusCode,
+    routing::{delete, get, post},
+    Json, Router,
+};
 use serde::Deserialize;
 use uuid::Uuid;
 
-use crate::config::Config;
+use crate::{
+    config::Config,
+    models::{score::Score, score_history::ScoreHistory},
+};
 
 #[derive(Clone)]
 pub struct AppState {
@@ -12,24 +20,44 @@ pub struct AppState {
 
 #[derive(Deserialize)]
 pub struct AddRequest {
-    key: String,  // max 32 chars
+    key: String, // max 32 chars
     value: f64,
 }
 
 #[derive(Deserialize)]
 pub struct RemoveRequest {
-    key: String,  // max 32 chars
+    key: String, // max 32 chars
 }
 
 #[derive(Deserialize)]
 pub struct HistoryQuery {
-    key: Option<String>,  // max 32 chars
+    key: Option<String>, // max 32 chars
     start: Option<String>,
     end: Option<String>,
 }
 
-async fn add_handler(State(_state): State<AppState>, Json(_req): Json<AddRequest>) {
-    todo!()
+async fn add_handler(
+    State(state): State<AppState>,
+    Json(req): Json<AddRequest>,
+) -> Result<Json<Score>, StatusCode> {
+    let mut uploader: String = req.key;
+    uploader.truncate(32);
+    let value: f64 = req.value;
+
+    Score::delete_by_uploader(&state.db, uploader.clone())
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    // TODO: Change these to reference
+    let new_score: Score = Score::create(&state.db, uploader.clone(), value)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    ScoreHistory::create(&state.db, uploader, value)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    Ok(Json(new_score))
 }
 
 async fn remove_handler(State(_state): State<AppState>, Json(_req): Json<RemoveRequest>) {
@@ -52,7 +80,7 @@ pub fn app(state: AppState) -> Router {
     Router::new()
         .route("/health", get(|| async { "OK" }))
         .route("/add", post(add_handler))
-        .route("/remove", post(remove_handler))
+        .route("/remove/{uploader}", delete(remove_handler))
         .route("/performance", get(performance_handler))
         .route("/info", get(info_handler))
         .route("/history", get(history_handler))
