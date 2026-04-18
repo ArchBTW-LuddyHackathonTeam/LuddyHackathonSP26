e0faaf4ce8519644af5e1719a93751d14c1679f8
lillie <onlinechronically@protonmail.com>
2026-04-17 20:41:58 -0400

    remove: #nuke leaderboard table

--- migrations/20260418000938_setup.sql
+++ migrations/20260418000938_setup.sql
@@ -1,13 +1,4 @@
 -- Add migration script here
-CREATE TABLE leaderboard
-(
-    id           UUID PRIMARY KEY   DEFAULT gen_random_uuid(),
-    display_name TEXT      NOT NULL,
-    private      BOOL      NOT NULL DEFAULT FALSE,
-    secret       TEXT      NOT NULL,
-    created_at   TIMESTAMP NOT NULL DEFAULT now()
-);
-
 CREATE TABLE score
 (
     id          UUID PRIMARY KEY   DEFAULT gen_random_uuid(),
