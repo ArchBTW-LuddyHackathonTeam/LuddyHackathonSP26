513650e77660e492f901d54c7515a717aef950ac
lillie <onlinechronically@protonmail.com>
2026-04-17 21:27:18 -0400

    add: value column+remove: leaderboard column

--- migrations/20260418000938_setup.sql
+++ migrations/20260418000938_setup.sql
@@ -1,8 +1,8 @@
 -- Add migration script here
 CREATE TABLE score
 (
-    id          UUID PRIMARY KEY   DEFAULT gen_random_uuid(),
-    leaderboard UUID      NOT NULL references leaderboard (id),
-    uploader    TEXT      NOT NULL DEFAULT 'Anonymous',
-    created_at  TIMESTAMP NOT NULL DEFAULT now()
+    id          UUID PRIMARY KEY          DEFAULT gen_random_uuid(),
+    uploader    TEXT             NOT NULL DEFAULT 'Anonymous',
+    created_at  TIMESTAMP        NOT NULL DEFAULT now(),
+    value       DOUBLE PRECISION NOT NULL
 );
